d974f15bcd6b1d864b5f06c156e6f2973d74332a
KaushalBhatol <kaushal.bhatol@outlook.com>
2021-08-15 14:09:07 +0530

    head and foot patch

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -4,9 +4,9 @@
     <p>Created by Kaushal Bharol &middot; &copy; <?=$year?></p>
   </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
-
+<!-- Js files  -->
+<script name="Bootstrap" src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script name="Bootstrap" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
 <!-- git md  -->
 <script>
 window.onload = function() {
@@ -27,6 +27,5 @@ window.onload = function() {
     }
 }
 </script>
-
 </body>
 </html>
--- assets/php/head.php
+++ assets/php/head.php
@@ -17,9 +17,8 @@
   <meta name="language" content="English">
   <meta name="author" content="<?=$author?>">
 
-  <!-- Github md css file  -->
-    <link href="<?=$root?>assets/css/github-markdown.css" rel="stylesheet" >
-  <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<!--CSS Files -->
+  <link naeme="Github MD file Style" href="<?=$css?>github-markdown.css" rel="stylesheet" >
+  <link naeme="Bootstrap css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
   </head>
   <body>
